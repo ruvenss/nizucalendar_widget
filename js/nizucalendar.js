aaa69fbfd1928497fa2c4d3167c6ab28b97e24ae
Ruvenss <ruvenss@gmail.com>
2019-08-25 19:46:16 +0200

    jquery

--- js/nizucalendar.js
+++ js/nizucalendar.js
@@ -1,4 +1,5 @@
 var nizucal_ver="0.14";
+
 var nizuapiserver="https://api.nizu.be/calendar/";
 var nizuapikey="";
 var nizuapiid="";
@@ -9,12 +10,19 @@ var nizucal_selectedday=0;
 var nizucal_selectedhour=12;
 var nizucal_selectedmin=0;
 var nizucal_today = new Date();
+var nuzucal_choosedestination=false;
 function nizucal_init(){
-    nizucal_selectedyear=today.getFullYear();
-    nizucal_selectedmonth=String(today.getMonth() + 1).padStart(2, '0');
-    nizucal_selectedday=String(today.getDate()).padStart(2, '0');
-    nizucal_selectedhour=today.getHours();
-    nizucal_selectedmin=today.getMinutes();
+    nizucal_selectedyear=nizucal_today.getFullYear();
+    nizucal_selectedmonth=String(nizucal_today.getMonth() + 1).padStart(2, '0');
+    nizucal_selectedday=String(nizucal_today.getDate()).padStart(2, '0');
+    nizucal_selectedhour=nizucal_today.getHours();
+    nizucal_selectedmin=nizucal_today.getMinutes();
+    nizucal_render();
+}
+function nizucal_render(){
+    console.log("rendering");
+    var nizucalendar = document.getElementsByClassName("nizucalendar");
+    console.log("render completed");
 }
 document.addEventListener("DOMContentLoaded", function(event) { 
     nizucal_init();
